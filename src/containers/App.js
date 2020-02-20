cbe805e919f34d9c6b46c86f6f7129d903562308
guptaaman92 <34260297+guptaaman92@users.noreply.github.com>
2020-02-20 23:48:55 +0530

    Commiting lifecycle changes
    changed files related to lifecycle changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-
 import classes from "./App.module.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
   state = {
     persons: [
       { id: "asfa1", name: "Max", age: 28 },
@@ -14,6 +17,19 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js");
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -47,6 +63,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
 
     if (this.state.showPersons) {
@@ -62,6 +79,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import App from "./containers/App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App appTitle="Person Manager" />, rootElement);
